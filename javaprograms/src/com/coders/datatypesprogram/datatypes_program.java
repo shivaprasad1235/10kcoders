d3d46d94c48059246b6c7673ae0aa32de733f093
t shivaprasad <tshivaprasad61@gmail.com>
2026-07-24 11:19:13 +0530

    datatypes

---
+++ javaprograms/src/com/coders/datatypesprogram/datatypes_program.java
@@ -0,0 +1,29 @@
+package com.coders.datatypesprogram;
+
+public class datatypes_program {
+	public static void main(String[] args) {
+		int a = 4;
+		char b = 'b';
+		String name = "Shiva";
+		float c = 98.0f;
+		double d = 98.9078d;
+		short e = 87;
+		boolean bl = true;
+		long g = 542378986876L;
+		System.out.println(a);
+		System.out.println(b);
+
+		System.out.println(name);
+
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(g);
+
+		
+		
+		
+		
+	}
+
+}
---
+++ javaprograms/src/com/coders/variableprogram/variableprogram.java
@@ -0,0 +1,13 @@
+package com.coders.variableprogram;
+import java.util.Scanner;
+
+public class variableprogram {
+
+	public static void main(String[] args) {
+		int Age= 10;
+		String name= "Shiva";
+		System.out.println(Age+name);
+		
+		
+	}
+}
